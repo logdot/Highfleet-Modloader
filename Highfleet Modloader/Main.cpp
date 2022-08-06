9000a44f7321280908ff1c07c443fe3ca93b1549
Timoteo Rider <Timoteo2.25@gmail.com>
2022-08-06 08:15:33 -0600

    Add better debug info to the modloader

--- Highfleet Modloader/Main.cpp
+++ Highfleet Modloader/Main.cpp
@@ -24,7 +24,7 @@ void main(int argc, char* argv[]) {
 	std::cout << "Try and create process" << std::endl;
 	if (!createSuccess) {
 		std::cout << "Process creation failed" << std::endl;
-		Sleep(5000);
+		Sleep(10000);
 		return;
 	}
 
@@ -34,11 +34,25 @@ void main(int argc, char* argv[]) {
 
 	//HANDLE ph = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_VM_WRITE, false, pi.dwProcessId);
 	HANDLE ph = pi.hProcess;
+	std::cout << "Trying to open process " << ph << std::endl;
+
 
 
 	LPVOID remoteBuffer = (LPVOID)VirtualAllocEx(ph, NULL, strlen(injectLibraryPath), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-	WriteProcessMemory(ph, remoteBuffer, injectLibraryPath, strlen(injectLibraryPath), NULL);
+	std::cout << "Trying to write into the process memory" << std::endl;
+	BOOL writeError = WriteProcessMemory(ph, remoteBuffer, injectLibraryPath, strlen(injectLibraryPath), NULL);
+	std::cout << "Writing into process memory returned " << writeError << std::endl;
+
 	LPTHREAD_START_ROUTINE addr = (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandleW(L"Kernel32.dll"), "LoadLibraryA");
 	//PTHREAD_START_ROUTINE loadLibraryFunction = (PTHREAD_START_ROUTINE)(GetProcAddress(GetModuleHandleW(L"Kernel32"), "LoadLibraryA"));
+	std::cout << "Trying to create a remote thread" << std::endl;
 	HANDLE remoteThreadHandle = CreateRemoteThread(ph, NULL, 0, addr, remoteBuffer, 0, NULL);
+	if (remoteThreadHandle == NULL)
+	{
+		std::cout << "Remote thread creation failed" << std::endl;
+	}
+	else {
+		std::cout << "Remote thread creation succeded" << std::endl;
+	}
+	Sleep(10000);
 }
