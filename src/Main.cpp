ed878d4086dace11ea50893d630fc78409c4ec96
Timoteo Rider <Timoteo2.25@gmail.com>
2022-09-01 21:33:16 -0600

    Load mods from folder

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,9 +1,38 @@
 #include <Windows.h>
 #include <iostream>
+#include <filesystem>
 #include "Inject.h"
+
+namespace fs = std::filesystem;
+
+void VerifyModFolders()
+{
+	if (!fs::exists("Modloader"))
+	{
+		std::cout << "Modloader folder does not exist. Creating it at `Modloader`" << std::endl;
+
+		fs::create_directories("Modloader");
+	}
+
+	if (!fs::exists("Modloader/mods"))
+	{
+		std::cout << "Mod folder does not exist. Creating it at `Modloader/mods`" << std::endl;
+
+		fs::create_directories("Modloader/mods");
+	}
+
+	if (!fs::exists("Modloader/config"))
+	{
+		std::cout << "Config folder does not exist. Creating it at `Modloader/config`" << std::endl;
+
+		fs::create_directories("Modloader/config");
+	}
+}
 
 int main(int argc, char *argv[])
 {
+	VerifyModFolders();
+
 	const char *injectLibraryPath = "ShipworksExtended.dll";
 
 	std::cout << "Attempt to launch game" << std::endl;
@@ -29,15 +58,21 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	std::cout << "Attempting to load mod" << std::endl;
-	bool injectSuccess = InjectLibrary(injectLibraryPath, ph);
-
-	if (!injectSuccess)
+	for (const auto &modEntry : fs::directory_iterator("Modloader/mods"))
 	{
-		std::cout << "Failed to load mod, error: " << GetLastError() << std::endl;
-		Sleep(EXIT_TIME);
-		return -1;
+		std::cout << "Attempting to load mod at: " << modEntry.path() << std::endl;
+		std::cout << (modEntry.path().string().c_str()) << std::endl;
+		bool injectSuccess = InjectLibrary((modEntry.path().string().c_str()), ph);
+
+		if (!injectSuccess)
+		{
+			std::cout << "Failed to load mod, error: " << GetLastError() << std::endl;
+			Sleep(EXIT_TIME);
+			return -1;
+		}
 	}
+
+	std::cout << "Finished loading all mods :)" << std::endl;
 
 	Sleep(EXIT_TIME);
 	return 0;
